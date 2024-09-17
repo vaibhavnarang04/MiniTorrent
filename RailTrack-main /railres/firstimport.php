23f6625a70bbd2d8f4607a926c3322e2d4671222
vaibhavnarang04 <vaibhav.narang@learner.manipal.edu>
2024-09-18 00:53:06 +0530

    Delete RailTrack-main /railres directory

--- RailTrack-main /railres/firstimport.php
+++
@@ -1,9 +0,0 @@
-<?php
-$host="localhost"; // Host name
-$username="root"; // Mysql username
-$password=""; // Mysql password
-$db_name="railres"; // Database name
-
-$conn=mysqli_connect("$host", "$username", "$password")or die("cannot connect");
-
-?>
